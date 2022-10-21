422e9f17c44012b371aa97d8e8efbb13cb4f5ee5
ivan <115603693+spoky232@users.noreply.github.com>
2022-10-21 05:06:52 +0300

    Add files via upload

---
+++ simmanennkolab7/src/programm1.java
@@ -0,0 +1,7 @@
+public class programm1 {
+    public static void main(String[] args) {
+        String []a = {"kiev", "lvov"};
+            System.out.println(a[1]);
+            System.out.println(a[0]);
+    }
+}
---
+++ simmanennkolab7/src/programm2.java
@@ -0,0 +1,5 @@
+public class programm2 {
+    public static void main(String[] args) {
+       HowMany.java
+    }
+}
